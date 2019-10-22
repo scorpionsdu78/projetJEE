86c5488354d65b08e9f90d22c64db7327a9293e5
Hugues Begeot <hugues.begeot@efrei.net>
2019-10-22 09:35:00 +0200

    Redoing the Constant.java and constants.properties + some Documentation

--- V1/JEE_PROJECT_V1/src/java/fr/efrei/API/Employee_get.java
+++ V1/JEE_PROJECT_V1/src/java/fr/efrei/API/Employee_get.java
@@ -8,13 +8,15 @@ package fr.efrei.API;
 import fr.efrei.dbcontroller.DBaction;
 import fr.efrei.jeeproject.Employee;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,8 +24,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author francois
  */
-public class Employee_get extends HttpServlet {
-
+public class Employee_get extends HttpServlet
+{
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,13 +36,17 @@ public class Employee_get extends HttpServlet {
      * @throws IOException if an I/O error occurs
      * @throws java.sql.SQLException
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-        
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException
+    {
         DBaction dba = new DBaction();
         ArrayList<Employee> employees = new ArrayList<Employee>(); 
-        
+
         ResultSet rs = dba.getResultSet("SELECT * FROM EMPLOYEE");
-        
+
+        // TESTING the getProperty method (the display is done within the method itself)
+        String pleaseDeleteMe = this.getProperty("ADMIN_LOGIN");
+
         while(rs.next()){
             Employee emp = new Employee();
             emp.setFirst_name(rs.getString("fisrt_name"));
@@ -52,13 +58,19 @@ public class Employee_get extends HttpServlet {
             employees.add(emp);
             
         }
-        
+
         request.setAttribute("empList", employees);
+<<<<<<< Updated upstream
         request.getRequestDispatcher("Employee/employees.jsp").forward(request, response);
         
         
         
+=======
+        request.getRequestDispatcher("employees.jsp");
+>>>>>>> Stashed changes
     }
+    
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -107,4 +119,29 @@ public class Employee_get extends HttpServlet {
         return "is this magic? yes it is ";
     }// </editor-fold>
 
+    
+    
+    /** Gets a property stored in the constant.properties file with a given index
+     * 
+     * @param propertyName Name of the property to return
+     * @return Returns a property from the constants.properties
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public String getProperty(String propertyName) throws ServletException, IOException
+    {   
+        String PROP_FILE_PATH = "/WEB-INF/db.properties";
+        InputStream input = getServletContext().getResourceAsStream(PROP_FILE_PATH);
+        
+        
+        Properties prop = new Properties();
+        prop.load(input);
+        
+        // DELETE ME WHEN THE METHOD IS WORKING
+        System.out.println(propertyName);
+        System.out.println(prop.getProperty(propertyName));
+        
+        
+        return prop.getProperty( propertyName );
+    }
 }
