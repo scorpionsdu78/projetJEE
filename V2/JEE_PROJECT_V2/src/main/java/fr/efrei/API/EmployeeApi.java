7252883620f50c2787f5aeda421cfbeb267f0041
DulcheE <34923261+DulcheE@users.noreply.github.com>
2019-11-11 17:49:15 +0100

    FIX

--- V2/JEE_PROJECT_V2/src/main/java/fr/efrei/API/EmployeeApi.java
+++ V2/JEE_PROJECT_V2/src/main/java/fr/efrei/API/EmployeeApi.java
@@ -33,16 +33,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "EmployeeApi.findAll", query = "SELECT e FROM EmployeeApi e")
-    , @NamedQuery(name = "EmployeeApi.findById", query = "SELECT e FROM EmployeeApi e WHERE e.id = :id")
-    , @NamedQuery(name = "EmployeeApi.findByFirstName", query = "SELECT e FROM EmployeeApi e WHERE e.first_name = :first_name")
-    , @NamedQuery(name = "EmployeeApi.findByLastName", query = "SELECT e FROM EmployeeApi e WHERE e.last_name = :last_name")
-    , @NamedQuery(name = "EmployeeApi.findByHomePhone", query = "SELECT e FROM EmployeeApi e WHERE e.home_phone = :home_phone")
-    , @NamedQuery(name = "EmployeeApi.findByCellPhone", query = "SELECT e FROM EmployeeApi e WHERE e.cell_phone = :cell_phone")
-    , @NamedQuery(name = "EmployeeApi.findByWorkPhone", query = "SELECT e FROM EmployeeApi e WHERE e.work_phone = :work_phone")
-    , @NamedQuery(name = "EmployeeApi.findByEMail", query = "SELECT e FROM EmployeeApi e WHERE e.email = :email")
-    , @NamedQuery(name = "EmployeeApi.findByRue", query = "SELECT e FROM EmployeeApi e WHERE e.rue = :rue")
-    , @NamedQuery(name = "EmployeeApi.findByCodePostal", query = "SELECT e FROM EmployeeApi e WHERE e.code_postal = :code_postal")
-    , @NamedQuery(name = "EmployeeApi.findByVille", query = "SELECT e FROM EmployeeApi e WHERE e.ville = :ville")})
+    , @NamedQuery(name = "EmployeeApi.findById", query = "SELECT e FROM EmployeeApi e WHERE e.id = :id")})
 public class EmployeeApi implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -109,51 +100,51 @@ public class EmployeeApi implements Serializable {
         this.id = id;
     }
 
-    public String getFirstName() {
+    public String getFirst_name() {
         return first_name;
     }
 
-    public void setFirstName(String first_name) {
+    public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
-    public String getLastName() {
+    public String getLast_name() {
         return last_name;
     }
 
-    public void setLastName(String last_name) {
+    public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
-    public String getHomePhone() {
+    public String getHome_phone() {
         return home_phone;
     }
 
-    public void setHomePhone(String home_phone) {
+    public void setHome_phone(String home_phone) {
         this.home_phone = home_phone;
     }
 
-    public String getCellPhone() {
+    public String getCell_phone() {
         return cell_phone;
     }
 
-    public void setCellPhone(String cell_phone) {
+    public void setCell_phone(String cell_phone) {
         this.cell_phone = cell_phone;
     }
 
-    public String getWorkPhone() {
+    public String getWork_phone() {
         return work_phone;
     }
 
-    public void setWorkPhone(String work_phone) {
+    public void setWork_phone(String work_phone) {
         this.work_phone = work_phone;
     }
 
-    public String getEMail() {
+    public String getEmail() {
         return email;
     }
 
-    public void setEMail(String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
