668d2b548a2a1f29315bc22030c0f844e69dd852
DulcheE <34923261+DulcheE@users.noreply.github.com>
2019-11-11 19:52:16 +0100

    FIX

--- V2/JEE_PROJECT_V2/src/main/java/fr/efrei/jpa/SB_Employee.java
+++ V2/JEE_PROJECT_V2/src/main/java/fr/efrei/jpa/SB_Employee.java
@@ -12,7 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -25,7 +25,7 @@ public class SB_Employee
     EntityManager em;
     EntityManagerFactory emf;
     
-    public List GET()
+    public List<EmployeeApi> GET()
     {   
         // We manage our Entity Managers
         emf = Persistence.createEntityManagerFactory("se.m1_JEE_PROJECT_V2_war_1.0PU");
@@ -33,11 +33,11 @@ public class SB_Employee
         
         
         // We do our SQL query
-        Query q = em.createNamedQuery("EmployeeApi.findAll", EmployeeApi.class);
+        TypedQuery q = em.createNamedQuery("EmployeeApi.findAll", EmployeeApi.class);
         
         
         // We initialize a list at null, then fill it with the query's results
-        List returnList = null;
+        List<EmployeeApi> returnList = null;
         returnList = q.getResultList();
         
         
@@ -49,4 +49,48 @@ public class SB_Employee
         // Return the list
         return returnList;
     }
+    
+    
+    public EmployeeApi GET(int id){
+        
+        emf = Persistence.createEntityManagerFactory("se.m1_JEE_PROJECT_V2_war_1.0PU");
+        
+        em = emf.createEntityManager();
+        
+        TypedQuery<EmployeeApi> q = em.createNamedQuery("EmployeeApi.findById", EmployeeApi.class);
+        
+        return q.getResultList().get(0);
+    }
+    
+    
+    public void PUT(int id, String last_name, String first_name, String home_pho, String mob_pho, String work_pho, String email, int idadd, String street, String postal, String city){
+        EmployeeApi employee = GET(id);
+        
+        if(last_name != null){
+            employee.setLast_name(last_name);
+        }
+        
+        if(first_name != null){
+            employee.setLast_name(first_name);
+        }
+        
+        if(home_pho != null){
+            employee.setLast_name(home_pho);
+        }
+        
+        if(mob_pho != null){
+            employee.setLast_name(mob_pho);
+        }
+        
+        if(work_pho != null){
+            employee.setLast_name(work_pho);
+        }
+        
+        if(email != null){
+            employee.setLast_name(email);
+        }
+        
+        SB_Adress sb_adress = new SB_Adress();
+        sb_adress.PUT(idadd, street, postal, city);
+    }    
 }
